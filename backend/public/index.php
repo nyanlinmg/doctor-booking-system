58aa8c71dfedaca6f3d77dd5da2fd589be2661c6
Nyan Lin Maung <nyanlinmg2666@gmail.com>
2026-04-08 20:39:33 +0630

    back to normal

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,13 +1,4 @@
 <?php
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit(0);
-}
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
