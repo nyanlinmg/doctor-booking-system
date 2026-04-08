7f4cea0ea14b176ddf7ec9344be2f5342759910f
Nyan Lin Maung <nyanlinmg2666@gmail.com>
2026-04-08 20:35:45 +0630

    add header

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
